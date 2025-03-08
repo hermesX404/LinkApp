aa82acea2cfdf22f828afb081f79d77bad29619e
HIYORI <130217681+HIYORI-S@users.noreply.github.com>
2025-03-08 19:40:06 +0900

    Initial Commit

---
+++ LinkTests/LinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  LinkTests.swift
+//  LinkTests
+//
+//  Created by 宋佳音 on 2025/03/08.
+//
+
+import Testing
+@testable import Link
+
+struct LinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
